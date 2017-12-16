227b2cb8858301391e9214b9faa51fd6905c0c70
fcmichael <michael.1@wp.pl>
2017-12-16 13:23:53 +0100

    Add log4j config

--- src/main/java/app/CurrencyRatesAnalyzer.java
+++ src/main/java/app/CurrencyRatesAnalyzer.java
@@ -1,11 +1,13 @@
 package app;
 
 import app.gui.CurrencyRatesAnalyzerFrame;
+import org.apache.log4j.Logger;
 
 public class CurrencyRatesAnalyzer {
 
     public static void main(String[] args) {
+        Logger.getRootLogger().info("Application start");
         new CurrencyRatesAnalyzerFrame();
+        Logger.getRootLogger().info("Application end");
     }
-
 }
